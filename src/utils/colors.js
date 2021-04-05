8e0d0e96ae628033cf0466bf12eafbe5b82e9116
Davin Ahn <davin.ahn@ridi.com>
2021-04-05 15:35:21 +0900

    Change primary color

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -5,4 +5,4 @@ export const transparent = 'transparent';
 export const greyBackground = '#ECEFF1';
 export const greyText = '#78909C';
 
-export const primaryColor = '#512DA8';
+export const primaryColor = '#1F8CE6';
